78be9cd9552b5254f14ed2cfcebf6cb0c3736251
Mithilesh Bhutada <48923599+miths5785@users.noreply.github.com>
2019-04-14 14:01:17 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,9 +1,9 @@
 <html>
 <head>
-	<meta charset = "utf-8">
+    <meta charset = "utf-8">
     <meta name = "viewport" content = "width = device-width, initial-scale = 1, shrink-to-fit = no">
     <link rel = "stylesheet" href = "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity = "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin = "anonymous">
-	<link rel = "stylesheet" type = "text/css" href = "loginstyle.css">
+    <link rel = "stylesheet" type = "text/css" href = "loginstyle.css">
 </head>
 <body>
 		<form method = "POST" action = "loginconfirm.php"> 
